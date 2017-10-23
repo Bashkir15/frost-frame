c5030c4fbf9dc158552c8e33c7fe9b5b564f5ac8
Forrest Collins <Forest.D.Collins@gmail.com>
2017-10-23 13:52:56 -0500

    caches map

--- packages/mapper/src/index.js
+++ packages/mapper/src/index.js
@@ -2,32 +2,37 @@ import Frame from '../../frame/src/index';
 import getSourceMap from '../../utils/src/sourceMap';
 
 const map = async (frames, fileUri, fileContents) => {
-	if (fileContents == null) {
-		fileContents = await fetch(fileUri)
-			.then(response => response.text());
-	}
+	const cache = {};
+	return await Promise.all(
+		frames.map(async frame => {
+			const { functionName, fileName, lineNumber, columnNumber } = frame;
+			let map = cache[fileName];
+			if (map == null) {
+				map[fileName] = await getSourceMap(
+					fileName,
+					await fetch(fileName).then(response => response.text());
+				)
+			}
 
-	const map = await getSourceMap(fileUri, fileContent);
-	return frames.map(frame => {
-		const { functionName, fileName, lineNumber, columnNumber } = frame;
-		const { source, line, column } = map.originalPositionFor({
-			line: lineNumber,
-			column: columnNumber
-		});
+			const { source, line, column } = map.originalPositionFor({
+				line: lineNumber,
+				column: columnNumber
+			});
 
-		return new Frame(
-			functionName,
-			fileName,
-			lineNumber,
-			columnNumber,
-			undefined,
-			functionName,
-			source,
-			line,
-			column,
-			undefined
-		);
-	});
+			return new Frame(
+				functionName,
+				fileName,
+				lineNumber,
+				columnNumber,
+				undefined,
+				functionName,
+				source,
+				line,
+				column,
+				undefined
+			)
+		})
+	)
 };
 
 export { map };
