875e4b87631534ed8d46b6a608dca272a29cbb56
Forrest Collins <Forest.D.Collins@gmail.com>
2017-10-23 13:44:53 -0500

    finds best path for partials

--- packages/unmapper/src/index.js
+++ packages/unmapper/src/index.js
@@ -1,16 +1,39 @@
 import Frame from '../../frame/src/index';
 import getSourceMap from '../../utils/src/sourceMap';
+import path from 'path';
 
 const unmap = async (frames, fileUri, fileContents) => {
 	if (fileContents == null) {
 		fileContents = await fetch(fileUri).then(response => response.text());
 	}
-	
+
 	const map = await getSourceMap(fileUri, fileContent);
 	return frames.map(frame => {
-		const { functionName, fileName, lineNumber, columnNumber } = frame;
+		const { functionName, lineNumber, columnNumber } = frame;
+		let { fileName } = frame;
+		if (fileName) {
+			fileName = path.resolve(fileName);
+		}
+
+		const sources = map.sources
+			.map(source => source.replace(/[\\]+/g, '/'))
+			.filter(source => {
+				source = path.resolve(source);
+				return source.indexOf(fileName) === source.length - fileName.length;
+			})
+			.map(source => source.split(path.sep))
+			.sort((a, b) => Math.sign(a.length - b.length))
+			.map(source => source.join(path.sep))
+			.map(source => source.split('node_modules'))
+			.sort((a, b) => Math.sign(a.length - b.length))
+			.map(source => source.join('node_modules'));
+
+		if (source.length < 1) {
+			return null;
+		}
+
 		const { line, column } = map.generatePositionFor({
-			source: fileName,
+			source: source[0],
 			line: lineNumber,
 			column: columnNumber
 		});
@@ -19,14 +42,12 @@ const unmap = async (frames, fileUri, fileContents) => {
 			functionName,
 			fileUri,
 			line,
-			column,
+			column || null,
 			undefined,
 			functionName,
-			fileName,
-			lineNumber,
-			columnNumber,
-			undefined
+			fileName
 		);
+
 	});
 }
 
