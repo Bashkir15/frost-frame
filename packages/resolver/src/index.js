44ace91d84e6896263eefa90a3c602bd619be023
Forrest Collins <Forest.D.Collins@gmail.com>
2017-10-23 11:21:08 -0500

    refactor mostly done. Three packages exist now

--- packages/resolver/src/index.js
+++ packages/resolver/src/index.js
@@ -1,59 +1,34 @@
-import ErrorStackParser from 'error-stack-parser';
+import Frame, { ScriptLine } from '../../frame/src/index';
+import { parse as parseError } from '../../parse/src/index';
 import { SourceMapConsumer } from 'source-map';
 
-class ResolvedFrame {
-	constructor(
-		functionName = '(anonymous function)',
-		fileName = '?',
-		lineNumber = 0,
-		columnNumber = 0,
-		scriptLines = [],
-		sourceFileName = '',
-		sourceLineNumber = 0,
-		sourceColumnNumber = 0,
-		sourceLines = []
-	) {
-		this.functionName = functionName;
-		this.fileName = fileName;
-		this.lineNumber = lineNumber;
-		this.columnNumber = columnNumber;
-		this.scriptLines = scriptLines;
-		this.sourceFileName = sourceFileName;
-		this.sourceLineNumber = sourceLineNumber;
-		this.sourceLines = sourceLines;
-	}
-}
-
 const awaitAll = async promises => {
-	for (const promise of promises) {
+	for (const promsise of promises) {
 		try {
 			await promise;
-		} catch (err) {}
+		} catch (err) {
+
+		}
 	}
 };
 
-const getLinesAround = (line, count, lines = []) => {
-	const result = [];
+const getLinesAround = (line, count, lines) => {
 	if (typeof lines === 'string') {
-		line = lines.split('\n');
+		lines = lines.split('\n');
 	}
 
-	const bound = Math.min(lines.length -1, line - 1 + count);
+	const bound = Math.min(lines.length - 1, line - 1 + count);
+	const result = [];
 	for (let i = Math.max(0, line - 1 - count); i < bound; i++) {
-		result.push({
-			text: lines[i],
-			line: i + 1,
-			context: i !== line - 1
-		});
+		result.push(new ScriptLine(i + 1, lines[i], i === line - 1));
 	}
-
 	return result;
 };
 
 const getSourceMap = async (file, contents) => {
 	const match = /\/\/[#@] ?sourceMappingURL=([^\s'"]+)\s*$/m.exec(contents);
 	if (!(match && match[1])) {
-		throw new Error(`Source map not found for file: ${file}`);
+		throw new Error(`Source map not found for ${file}`);
 	}
 
 	let sourceMap = match[1].toString();
@@ -61,28 +36,28 @@ const getSourceMap = async (file, contents) => {
 		const base64 = /^data:application\/json;([\w=:"-]+;)*base64,/;
 		const match2 = sourceMap.match(base64);
 		if (!match2) {
-			throw new Error('Sorry, this inline source-map encoding is not supported');
+			throw new Error('Sorry, non base64 inline source-map encoding is not supported');
 		}
 
 		sourceMap = sourceMap.substring(match2[0].length);
 		sourceMap = window.atob(sourceMap);
 		sourceMap = JSON.parse(sourceMap);
-		return new sourceMapConsumer(sourceMap);
+		return new SourceMapConsumer(sourceMap);
 	} else {
-		const index = file.lastIndxOf('/');
+		const index = file.lastIndexOf('/');
 		const url = file.substring(0, index + 1) + sourceMap;
-		const obj = await fetch(url).then(result => result.json());
-		return new sourceMapConsumer(obj);
+		const obj = await fetch(url).then(response => response.json());
+		return new SourceMapConsumer(obj);
 	}
-}
+};
 
 const resolve = async (error, context = 3) => {
-	const frames = ErrorStackParser.parse(error);
+	const frames = parseError(error);
 	const files = {};
 
 	for (const frame of frames) {
 		const { fileName } = frame;
-		if (fileName == null || (typeof fileName) !== 'string') {
+		if (fileName == null || typeof fileName !== 'string') {
 			continue;
 		}
 		files[fileName] = null;
@@ -95,8 +70,10 @@ const resolve = async (error, context = 3) => {
 		try {
 			requests.push(
 				fetch(file)
-					.then(res => res.text())
-					.then(text => files[file] = text)
+					.then(response => response.text())
+					.then(text => {
+						files[file] = text;
+					})
 					.catch(err => {})
 			);
 		} catch (err) {
@@ -109,23 +86,32 @@ const resolve = async (error, context = 3) => {
 	const sourcemaps = {};
 	requests = [];
 	for (const file of fileList) {
-		request.push(getSourceMap(file, files[file]))
-			.then(map => {
-				sourcemaps[file] = map;
-			})
-			.catch(e => {})
+		requests.push(
+			getSourceMap(file, files[file])
+				.then(map => {
+					sourcemaps[file] = map;
+				})
+				.catch(err => {})
+		);
 	}
 
 	await awaitAll(requests);
 
 	const resolved = [];
 	for (let i = 0; i < frames.length; i++) {
-		const { [index]: { functionName, fileName, lineNumber: line, columnNumber: column }} = frames;
-		resolved[index] = new ResolvedFrame(functionName, fileName, line, column);
-
+		const {
+			[index]: {
+				functionName,
+				fileName,
+				lineNumber: line,
+				columnNUmber: column
+			}
+		} = frames;
+		resolved[index] = new Frame(functionName, fileName, line, column);
 		if (fileName == null || line == null || column == null) {
 			continue;
 		}
+
 		if (!files.hasOwnProperty(fileName)) {
 			continue;
 		}
@@ -135,42 +121,52 @@ const resolve = async (error, context = 3) => {
 			continue;
 		}
 
-		const oScriptArr = getLinesAround(line, context, script);
-		resolved[index] = new ResolvedFrame(functionName, fileName, line, column, oScriptArr);
-
-		if (!sourcemaps.hasOwnProperty(filename)) {
-			continue;
-		}
-
-		const { [fileName]: map } = sourcemaps;
-		if (map == null) {
-			continue;
-		}
-
-		const original = map.originalPositionFor({ line, column });
-		const { source: sourceFile, line: sourceLine, column: sourceColumn } = original;
-		if (!sourceFile || !line) {
-			continue;
-		}
-
-		const originalSource = map.sourceContentsFor(sourceFile);
-		const oSourceArr = getLinesAround(sourceLine, context, originalSource);
-
-		resolve[index] = new ResolvedFrame(
+		const originalScriptArr = getLinesAround(line, context, script);
+		resolved[index] = new Frame(
 			functionName,
 			fileName,
 			line,
 			column,
-			oScriptArr,
+			originalScriptArr
+		);
+
+		if (!sourcemaps.hasOwnProperty(fileName)) {
+			continue;
+		}
+
+		const { [filename]: map } = sourcemaps;
+		if (map == null) {
+			continue;
+		}
+		const original = map.originalPositionFor({ line, column });
+		const {
+			source: sourceFile,
+			line: sourceFile,
+			column: sourceColumn
+		} = original;
+
+		if (!sourceFile || !line) {
+			continue;
+		}
+
+		const originalSource = map.sourceContentFor(sourceFile);
+		const originalSourceArr = getLinesAround(sourceLine, context, originalSource);
+		resolved[index] = new Frame(
+			functionName,
+			fileName,
+			line,
+			column,
+			originalScriptArr,
+			functionName,
 			sourceFile,
 			sourceLine,
 			sourceColumn,
-			oSourceArr
+			originalSourceArr
 		);
 	}
 
 	return resolved;
 }
 
+export { resolve };
 export default resolve;
-export { resolve, ResolvedFrame }
